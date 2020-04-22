8a21531ff4495d0a9cf7ec137e3bf8335a630e1f
zareanmasoud <zareanmasoud@gmail.com>
2020-04-22 20:42:17 +0100

    add accessibilityService

---
+++ android/src/main/java/com/reactlibrary/MyAccessibilityService.java
@@ -0,0 +1,23 @@
+package com.reactlibrary;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+public class MyAccessibilityService extends AccessibilityService {
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+        AccessibilityNodeInfo source = accessibilityEvent.getSource();
+        if (source == null) {
+            return;
+        }
+
+        AccessibilityServiceModule.prepareEvent(accessibilityEvent.toString());
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
